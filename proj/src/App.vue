cacc28f605e5b80bf0b174b1c9045468bdba871a
Yona Voss-Andreae <yonavossandreae@gmail.com>
2021-12-11 01:47:14 -0500

    v1.1

--- proj/src/App.vue
+++ proj/src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+		<center>
+
+			<div v-if="selected === 'paths'">
+				<Paths />
+			</div>
+			<div v-else-if="selected === 'complete'">
+				<Complete />
+			</div>
+			<div v-else-if="selected === 'cycle'">
+				<Cycles />
+			</div>
+
+		
+			<div v-else class="main-container">
+				<h1>Choose Your Poison:</h1>
+				<button v-on:click="switchPage('paths')">Pebble a Path Graph</button>
+				<button v-on:click="switchPage('complete')">Pebble a Complete Graph</button>
+				<button v-on:click="switchPage('cycle')">Pebble a Cycle Graph</button>
+			</div>
+
+		</center>
+		
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Paths from './components/Paths.vue'
+import Complete from './components/Complete.vue'
+import Cycles from './components/Cycles.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		Paths,
+		Complete,
+		Cycles
+	},
+	data: () => {
+		return {
+			selected: '',
+		}
+	},
+	mounted() {
+
+	},
+	watch: {
+	
+	},
+	methods: {
+		switchPage(selected) {
+			this.selected = selected
+		}
+	},
+	computed: {
+
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main-container {
+	/* border: 5px solid black; */
+}
+button {
+	background-color: white;
+	border-radius: 25px;
+	margin: 2.5%;
+	padding: 3%;
+	font-size: 1.5rem;
+	cursor: pointer;
+	font-family: monospace;
+}
+button:hover {
+	background-color: rgb(100, 196, 228);
+}
+input {
+	width: 25vw;
+	font-size: 20pt;
+	padding: 1%;
+	border-radius: 10px;
+}
+body {
+	font-family: monospace;
+	font-size: 1.5rem;
+	background-color: rgb(240, 240, 240);
+}
+.home {
+	position: sticky;
+	bottom: 0;
 }
 </style>
---
+++ proj/src/components/Paths.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Path Pebbling</h1>
+        <hr>
+        <h2>Input Number of Vectors:</h2>
+        <input type="number" v-model="vectors" />
+        <h2>Pebbling Number - {{ minPebbles }}</h2>
+        <button class="home" v-on:click="$parent.switchPage('')">Home</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            vectors: 0,
+            minPebbles: 'Invalid',
+        }
+    },
+    watch: {
+        vectors() {
+            this.vectors > 1 ? this.minPebbles = 2**(this.vectors-1):this.minPebbles = 'Invalid';
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    padding: .5%;
+    width: 20vw;
+}
+button:hover {
+    background-color: lightcoral;
+}
+h2 {
+    font-weight: normal;
+}
+
+</style>
